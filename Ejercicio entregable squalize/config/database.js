76e885971c5784a77c4297dc426be4bd14d75216
matute <idkmatt0@gmail.com>
2025-04-11 19:03:43 -0300

    old version

--- Ejercicio entregable squalize/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("ejercicio_api_sequelize", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
-
-module.exports = sequelize;
--- Ejercicio entregable squalize/routes/articulos.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const articuloController = require("../controllers/articuloController");
-
-router.get("/", articuloController.getArticulos);
-
-module.exports = router;
